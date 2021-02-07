f5071d56d0fa518fb54469e7c2004e2b679e6cc4
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-02-07 16:45:35 +0900

    コードとstory boardの紐付け

--- TaskPart2/ViewController.swift
+++ TaskPart2/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var label1: UITextField!
+    @IBOutlet private weak var label2: UITextField!
+    @IBOutlet private weak var segementedControl: UISegmentedControl!
+    @IBOutlet private weak var label: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonAction(_ sender: UIButton) {
+    }
 
 }
 
